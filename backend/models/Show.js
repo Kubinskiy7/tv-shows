4c00ab1a2d266c0db3a3ac9af505728a04fe074f
Kubinskiy7 <136919214+Kubinskiy7@users.noreply.github.com>
2024-11-21 23:28:02 +0500

    Update Show.js

--- backend/models/Show.js
+++ backend/models/Show.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
 
+const ShowSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  poster: { type: String, required: true },
+  seasons: [
+    {
+      seasonNumber: Number,
+      episodes: [
+        {
+          episodeNumber: Number,
+          title: String,
+          rating: Number,
+        },
+      ],
+    },
+  ],
+});
+
+module.exports = mongoose.model('Show', ShowSchema);
