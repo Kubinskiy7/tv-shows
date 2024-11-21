1a92edac32ba13ea48f623bcc0abe2401857484f
Kubinskiy7 <136919214+Kubinskiy7@users.noreply.github.com>
2024-11-22 00:22:35 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const mongoURI = process.env.MONGO_URI;
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error(err.message);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
